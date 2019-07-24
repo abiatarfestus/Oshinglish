8486cad283949952d63036a85dfa9539beb519ca
abiatarfestus <40578707+abiatarfestus@users.noreply.github.com>
2019-07-24 02:22:34 +0200

    WRL0003 deleted

--- Oshinglish.py
+++ Oshinglish.py
@@ -5,26 +5,27 @@ root = Tk()
 root.title("Oshinglish Dictionary First Edition")
 root.geometry("600x500")
 
-nameWithLogo = ttk.Label(root, text = "Oshinglish Dictionary First Edition", background = "blue")
-nameWithLogo.grid(rowspan = 3, sticky = W+E)
+nameWithLogo = ttk.Label(root, text = "Oshinglish Dictionary First Edition", font = "Times 28", background = "blue")
+nameWithLogo.grid(columnspan = 20, rowspan = 5, sticky = W)
 
 options = ttk.OptionMenu(root, variable = 3)
-options.grid(column = 0, row = 3, sticky = W)
+options.grid(columnspan = 3, row = 5, sticky = W)
 
 infoMenu = ttk.Menubutton(root)
-infoMenu.grid(column = 1, row = 3)
+infoMenu.grid(column = 3, row = 5)
 
 searchBox = ttk.Entry(root)
-searchBox.grid(columnspan = 3, row = 4, sticky = W)
+searchBox.grid(columnspan = 11, row = 6, sticky = W)
 
 findButton = ttk.Button(root, text = "Find")
-findButton.grid(column = 3, row = 4)
+findButton.grid(column = 9, row = 6)
 
 listLabel = ttk.Label(root, text = "List of words appears here", background = "white")
-listLabel.grid(columnspan = 3, rowspan = 15, sticky = W)
+listLabel.grid(columnspan = 5, rowspan = 15, sticky = W)
+
 
 definitionSpace = ttk.Label(root, text = "Definitions appear here")
-definitionSpace.grid(columnspan = 5, rowspan = 5)
+definitionSpace.grid(columnspan = 15, rowspan = 15)
 
 
 
