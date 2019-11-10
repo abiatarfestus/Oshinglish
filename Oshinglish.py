ce013f352d65b1e6787d9ae5962c2f46c98d450f
Festus Abiatar <abiatarfestus@outlook.com>
2019-11-10 23:11:35 +0200

    Commit all

--- Oshinglish.py
+++ Oshinglish.py
@@ -1,31 +1,31 @@
-from tkinter import *
-from tkinter import ttk
+from tkinter import*
 
 root = Tk()
 root.title("Oshinglish Dictionary First Edition")
-root.geometry("600x500")
+root.configure(background = "#0970d2")
+root.geometry("800x800")
 
-nameWithLogo = ttk.Label(root, text = "Oshinglish Dictionary First Edition", font = "Times 28", background = "blue")
-nameWithLogo.grid(columnspan = 20, rowspan = 5, sticky = W)
+nameWithLogo = Label(root, text = "Oshinglish Dictionary First Edition", font = "Times 28", background = "#982901")
+nameWithLogo.grid()
 
-options = ttk.OptionMenu(root, variable = 3)
-options.grid(columnspan = 3, row = 5, sticky = W)
+#options = OptionMenu(root, variable = 3)
+#options.grid(columnspan = 3, row = 5, sticky = W)
 
-infoMenu = ttk.Menubutton(root)
+infoMenu = Menubutton(root)
 infoMenu.grid(column = 3, row = 5)
 
-searchBox = ttk.Entry(root)
+searchBox = Entry(root)
 searchBox.grid(columnspan = 11, row = 6, sticky = W)
 
-findButton = ttk.Button(root, text = "Find")
+findButton = Button(root, text = "Find")
 findButton.grid(column = 9, row = 6)
 
-listLabel = ttk.Label(root, text = "List of words appears here", background = "white")
-listLabel.grid(columnspan = 5, rowspan = 15, sticky = W)
+listLabel = Label(root, text = "List of words appears here", background = "white", height = 40)
+listLabel.grid()
 
 
-definitionSpace = ttk.Label(root, text = "Definitions appear here")
-definitionSpace.grid(columnspan = 15, rowspan = 15)
+#definitionSpace = Label(root, text = "Definitions appear here")
+#definitionSpace.grid(columnspan = 15, rowspan = 15)
 
 
 
